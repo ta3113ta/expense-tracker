eafb6795d7094a12252dddce477f6c751d647c3a
ta3113ta <wuttichai3113@gmail.com>
2021-06-04 19:50:40 +0700

    add post transection

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,13 @@
-export class CreateTransactionDto {}
+import { IsString, IsDateString, IsNumber, IsNotIn } from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsString()
+  text: string;
+
+  @IsNumber()
+  @IsNotIn([0])
+  amount: number;
+
+  @IsDateString()
+  date: Date;
+}
--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -8,19 +8,32 @@ import {
   Delete,
   Request,
   UseGuards,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { UserGuard } from 'users/user.guard';
+import { ValidationPipe } from 'validation.pipe';
 
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
+  @UseGuards(UserGuard)
+  @HttpCode(HttpStatus.CREATED)
   @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionsService.create(createTransactionDto);
+  async create(
+    @Body(new ValidationPipe()) createTransactionDto: CreateTransactionDto,
+    @Request() req,
+  ) {
+    const { userId } = req.user;
+    const transection = await this.transactionsService.create(
+      createTransactionDto,
+      userId,
+    );
+    return { success: true, data: transection };
   }
 
   @UseGuards(UserGuard)
